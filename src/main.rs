11aa414fabaf9348ece1bef0729cef2c88f59f79
Lucas Code <lucascode.ctt@gmail.com>
2026-02-07 00:23:01 -0300

    feat: add pause for windows terminal support

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ mod jogo;
 mod ui;
 mod util;
 
+use rpassword::prompt_password;
+
 fn main() {
     loop {
         if jogo::logica_jogo() {
             continue;
         } else {
             println!("[>] Obrigado por jogar! - @lucazcode\n");
+            let _ = prompt_password("[?] Pressione ENTER para sair...");
+            println!();
             break;
         }
     }
